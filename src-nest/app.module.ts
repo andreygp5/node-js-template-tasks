f8373f67a538dce050b08acc08d6a783c6fb41e1
andreygp5 <andreygp555@gmail.com>
2021-07-06 23:03:05 +0300

    feat: configure typeorm

--- src-nest/app.module.ts
+++ src-nest/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { configSchema } from './common/joi.config.schema';
 
 @Module({
-  imports: [
+  imports:
+  [
     ConfigModule.forRoot({
-    validationSchema: configSchema,
-  })
-],
+      validationSchema: configSchema,
+    }),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src-nest/common/joi.config.schema.ts
+++ src-nest/common/joi.config.schema.ts
@@ -5,17 +5,6 @@ const configSchema = Joi.object({
     .valid('development', 'production')
     .default('development'),
   PORT: Joi.number().default(4000),
-
-  TYPEORM_CONNECTION: Joi.string().default('postgres'),
-  TYPEORM_HOST: Joi.string().default('localhost'),
-  TYPEORM_USERNAME: Joi.string().default('postgres'),
-  TYPEORM_PASSWORD: Joi.string().default('secret'),
-  TYPEORM_DATABASE: Joi.string().default('trello-competitor'),
-  TYPEORM_PORT: Joi.number().default(5432),
-  TYPEORM_LOGGING: Joi.boolean().default(true),
-  TYPEORM_ENTITIES: Joi.string().default('src-nest/**/*.entity{ .ts,.js})'),
-  TYPEORM_MIGRATIONS: Joi.string().default('migrations/*.ts'),
-
   JWT_SECRET_KEY: Joi.string(),
 })
 
